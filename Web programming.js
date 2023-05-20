bbb4b9a468aa0d9b9546e592b97bcc05cdb0b098
Sasithavinda4 <sasavind2021@gmail.com>
2023-05-20 11:52:34 +0530

    first commit

---
+++ Web programming.js
@@ -0,0 +1,4 @@
+let bot_name = "Robot";
+let year = 2023;
+console.log("Hello! My name is " + bot_name);
+console.log("I was created in " + year);
---
+++ p3.js
@@ -0,0 +1,6 @@
+const pi = 3.142;
+let r = 5;
+let area = pi * r * r;
+let perimeter = 2 * pi * r;
+console.log(area);
+console.log(perimeter);
